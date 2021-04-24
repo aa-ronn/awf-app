890a2ff52902bc3609e4ef6082d797ca3319313e
John <jstanley1339@conestogac.on.ca>
2021-04-24 12:48:36 -0400

    Tasks page implemented

--- src/pages/tasks/tasks.page.tsx
+++ src/pages/tasks/tasks.page.tsx
@@ -1,11 +1,29 @@
+import { useContext } from "react";
+import { StoreContext } from "../../context/store/store.context";
 import { Fab } from "../../components/fab/fab.component";
+import { Card } from "../../components/Card/card.component";
 import "./tasks.styles.scss";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 export const TasksPage = () => {
+  const { projects } = useContext(StoreContext);
   return (
     <div className="tasks-page">
       <div className="content">
+        {projects &&
+          projects.map((project, index) => {
+            return (
+              <div key={index}>
+                <Card
+                  id={project.id}
+                  type="task"
+                  title={project.title}
+                  line1={project.created}
+                  line2={project.description}
+                />
+              </div>
+            );
+          })}
         <Fab
           icon={faPlus}
           text="Task"
